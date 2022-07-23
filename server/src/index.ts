638c8ea0d9caa40be0a764424ccdc971e9ff7423
Cavan Law <cavan.law@viooh.com>
2022-07-23 18:49:47 +0100

    add server files and db connection

---
+++ server/src/index.ts
@@ -0,0 +1,32 @@
+import 'module-alias/register';
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+// connect to mongodb atlas.
+const DB_URL =
+  'mongodb+srv://tutorial-mern:<password>@cluster0.lflbvty.mongodb.net/?retryWrites=true&w=majority';
+
+// parses our body requests.
+app.use(bodyParser.json({ limit: '30mb' }));
+app.use(bodyParser.urlencoded({ limit: '30mb' }));
+// allows cors.
+app.use(cors());
+
+mongoose
+  .connect(DB_URL)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
